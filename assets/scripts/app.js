391d8431b8898452bcf01781d5ef46e2e66292b0
Matan <matantoled@gmail.com>
2025-06-05 14:29:39 +0300

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+import {apiKey} from './util.js';
+import www from "./util.js";
+
+import {abc, def} from "./util.js";
+
+import * as personalData from "./util.js";
+
+console.log(apiKey);
+console.log(www);
+
+console.log(personalData.ID);
---
+++ assets/scripts/util.js
@@ -0,0 +1,18 @@
+////// ways to import/export data //////////
+
+// first way: simple
+export let apiKey = 'wsadwasdawd';
+
+// second: when you want to export only one thing
+export default "wwwww";
+// export default "123"; => can't be done bcs we have one already
+
+// third: we can export multiple variables:
+export let abc = "abc";
+export let def = "def";
+
+
+// third 2.0 : we can export multiple variables and the import side will receive this in one package
+export let firstName = "Matan";
+export let lastName = "Toledano";
+export let ID = 123456;
